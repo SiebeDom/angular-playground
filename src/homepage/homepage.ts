f594f97de681c2cd33f6e26659c1c8cf0cecde42
SiebeDom <siebe.dom@gmail.com>
2026-02-21 19:07:33 +0100

    Library setup

--- src/homepage/homepage.ts
+++ src/homepage/homepage.ts
@@ -1,11 +1,14 @@
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {Router} from '@angular/router';
 import {Button} from 'primeng/button';
+import {
+  AngularLibraryPlayground
+} from '../../../../angular-library-playground/angular-library-playground-workspace/dist/angular-library-playground';
 
 @Component({
   selector: 'app-homepage',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [Button],
+  imports: [Button, AngularLibraryPlayground],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
